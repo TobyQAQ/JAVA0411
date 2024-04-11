e2a3623b692ff0bdf6045570ff04edf7207c49af
TobyQAQ <a0988065874@gmail.com>
2024-04-11 14:03:04 +0800

    240411_2
    1.一維二維陣列的建立
    2.利用迴圈把數字寫入陣列

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] a = {1, 2, 3, 4, 5};
+        System.out.println(a.length);
+
+        int[] b = new int[6];
+        b[0] = 7;
+        b[1] = 6;
+        b[2] = 5;
+
+        int[][] c = {{1, 1}, {2, 2}, {3, 3}};
+        System.out.print(c[2][1]);
+        int[][] d = new int[3][2];
+        d[1][0] = 3;
+        d[1][1] = 3;
+        d[2][0] = 4;
+        d[2][1] = 4;
+        d[0][0] = 7;
+        d[0][1] = 7;
+        System.out.println(d[2][0]);
+
+        int[] e = new int[10];
+        for (int i = 0; i <= 9; i++) {
+            e[i] = i + 1;
+            System.out.println(e[i]);
+        }
+
     }
 }
